f487fd3d963636e607252b08b41d6c7c01447483
SalvoCab99 <salvatore.cabras@studenti.polito.it>
2023-12-29 19:06:54 +0100

    some matches

--- Quixo/main.py
+++ Quixo/main.py
@@ -12,20 +12,26 @@ class RandomPlayer(Player):
         return from_pos, move
 
 
-class MyPlayer(Player):
+class RealPlayer(Player):
     def __init__(self) -> None:
         super().__init__()
 
     def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
-        from_pos = (4, 4)
-        move = random.choice([Move.LEFT])
-        return from_pos, move
+        # Leggi le coordinate da tastiera
+        from_pos_input = tuple(map(int, input("Inserisci le coordinate (x y): ").split()))
+
+        # Leggi la mossa da tastiera
+        move_input_str = input("Inserisci la mossa (TOP, BOTTOM, LEFT, RIGHT): ").upper()
+        move_input = Move[move_input_str]
+        print(from_pos_input)
+
+        return from_pos_input, move_input
 
 
 if __name__ == '__main__':
     g = Game()
     g.print()
-    player1 = MyPlayer()
+    player1 = RandomPlayer()
     player2 = RandomPlayer()
     winner = g.play(player1, player2)
     g.print()
